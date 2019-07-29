daf4364b805a98d1fd233e61efeaf538a92fb560
Rosen Runtev <runtev.rosen@gmail.com>
2019-07-29 14:18:20 +0300

    Added javadoc to GuestService and added method to get a guest by id

--- src/main/java/eu/deltasource/internship/hotel/service/GuestService.java
+++ src/main/java/eu/deltasource/internship/hotel/service/GuestService.java
@@ -1,15 +1,29 @@
 package eu.deltasource.internship.hotel.service;
 
+import eu.deltasource.internship.hotel.domain.Guest;
 import eu.deltasource.internship.hotel.repository.GuestRepository;
 
 /**
- * Created by Taner Ilyazov - Delta Source Bulgaria on 2019-07-28.
+ * A class that represents service for guests
  */
 public class GuestService {
 
-    private final GuestRepository guestRepository;
+	private final GuestRepository guestRepository;
 
-    public GuestService(GuestRepository guestRepository) {
-        this.guestRepository = guestRepository;
-    }
+	/**
+	 * Constructs a service with given guest repository
+	 * @param guestRepository repository with guests
+	 */
+	public GuestService(GuestRepository guestRepository) {
+		this.guestRepository = guestRepository;
+	}
+
+	/**
+	 * Returns the guest with particular id
+	 * @param id the id of the guest
+	 * @return the guest with particular id
+	 */
+	public Guest getGuestById(int id) {
+		return guestRepository.findById(id);
+	}
 }
