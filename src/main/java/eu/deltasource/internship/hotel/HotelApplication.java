f8bf4e87e2cb7527e8c9064497c83fb54e0f6502
Rosen Runtev <runtev.rosen@gmail.com>
2019-07-30 15:11:23 +0300

    Changed main class to run spring application

--- src/main/java/eu/deltasource/internship/hotel/HotelApplication.java
+++ src/main/java/eu/deltasource/internship/hotel/HotelApplication.java
@@ -1,14 +1,18 @@
 package eu.deltasource.internship.hotel;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by Taner Ilyazov - Delta Source Bulgaria on 2019-07-28.
  */
+@SpringBootApplication
 public class HotelApplication {
 
-    /**
-     * #thisisamain
-     */
-    public static void main(String[] args) {
-
-    }
+	/**
+	 * #thisisamain
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(HotelApplication.class, args);
+	}
 }
