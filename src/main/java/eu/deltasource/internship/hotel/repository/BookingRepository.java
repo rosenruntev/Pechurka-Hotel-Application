97776eb559e788cc46ecce7f1d47579a34b792c7
Rosen Runtev <runtev.rosen@gmail.com>
2019-07-30 10:52:27 +0300

    Fixing bugs in repositories

--- src/main/java/eu/deltasource/internship/hotel/repository/BookingRepository.java
+++ src/main/java/eu/deltasource/internship/hotel/repository/BookingRepository.java
@@ -13,123 +13,135 @@ import java.util.List;
  */
 public class BookingRepository {
 
-    private final List<Booking> repository;
+	private final List<Booking> repository;
 
-    /**
-     * Default constructor, which initializes the repository
-     * as an empty ArrayList.
-     */
-    public BookingRepository() {
-        repository = new ArrayList<>();
-    }
+	/**
+	 * Default constructor, which initializes the repository
+	 * as an empty ArrayList.
+	 */
+	public BookingRepository() {
+		repository = new ArrayList<>();
+	}
 
-    /**
-     * Returns an unmodifiable list of all items
-     * currently in the repository.
-     */
-    public List<Booking> findAll() {
-        return Collections.unmodifiableList(repository);
-    }
+	/**
+	 * Returns an unmodifiable list of all items
+	 * currently in the repository.
+	 */
+	public List<Booking> findAll() {
+		return Collections.unmodifiableList(repository);
+	}
 
-    /**
-     * Method, which checks the repository if
-     * there is an item available with the given id.
-     *
-     * Check this always, before using operations with id's.
-     */
-    public boolean existsById(int id) {
-        for (Booking item : repository) {
-            if (item.getBookingId() == id)
-                return true;
-        }
-        return false;
-    }
+	/**
+	 * Method, which checks the repository if
+	 * there is an item available with the given id.
+	 * <p>
+	 * Check this always, before using operations with id's.
+	 */
+	public boolean existsById(int id) {
+		for (Booking item : repository) {
+			if (item.getBookingId() == id)
+				return true;
+		}
+		return false;
+	}
 
-    /**
-     * Returns an item from the repository
-     */
-    public Booking findById(int id) {
-        for (Booking item : repository) {
-            if (item.getBookingId() == id)
-                return new Booking(item);
-        }
-        throw new ItemNotFoundException("A booking with id: " + id + " was not found!");
-    }
+	/**
+	 * Returns an item from the repository
+	 */
+	public Booking findById(int id) {
+		for (Booking item : repository) {
+			if (item.getBookingId() == id)
+				return new Booking(item);
+		}
+		throw new ItemNotFoundException("A booking with id: " + id + " was not found!");
+	}
 
-    /**
-     * Saves the item in the repository with a new id
-     * using the item count in the repository
-     */
-    public void save(Booking item) {
-        Booking newBooking = new Booking(count() + 1, item.getGuestId(), item.getRoomId(),
-                item.getNumberOfPeople(), item.getFrom(), item.getTo());
-        repository.add(newBooking);
-    }
+	/**
+	 * Saves the item in the repository with a new id
+	 * using the item count in the repository
+	 */
+	public void save(Booking item) {
+		Booking newBooking = new Booking(idGenerator(), item.getGuestId(), item.getRoomId(),
+			item.getNumberOfPeople(), item.getFrom(), item.getTo());
+		repository.add(newBooking);
+	}
 
-    /**
-     * Saves the list of items in the repository
-     */
-    public void saveAll(List<Booking> items) {
-        items.forEach(
-                this::save);
-    }
+	/**
+	 * Saves the list of items in the repository
+	 */
+	public void saveAll(List<Booking> items) {
+		items.forEach(
+			this::save);
+	}
 
-    /**
-     * Saves all given items in the repository
-     */
-    public void saveAll(Booking... items) {
-        saveAll(Arrays.asList(items));
-    }
+	/**
+	 * Saves all given items in the repository
+	 */
+	public void saveAll(Booking... items) {
+		saveAll(Arrays.asList(items));
+	}
 
-    /**
-     * Updates the dates of a given booking
-     * <p>
-     * You can only update the Date of a booking,
-     * if you need to update something else a new
-     * Booking has to be created and this one needs to be removed.
-     * <p>
-     * All validations should be done in the service layer!!!
-     */
-    public Booking updateDates(Booking item) {
-        Booking updatedBooking = findById(item.getBookingId());
-        updatedBooking.setBookingDates(item.getFrom(), item.getTo());
-        return updatedBooking;
-    }
+	/**
+	 * Updates the dates of a given booking
+	 * <p>
+	 * You can only update the Date of a booking,
+	 * if you need to update something else a new
+	 * Booking has to be created and this one needs to be removed.
+	 * <p>
+	 * All validations should be done in the service layer!!!
+	 */
+	public Booking updateDates(Booking item) {
+		for (Booking booking : repository) {
+			if (booking.getBookingId() == item.getBookingId()) {
+				booking.setBookingDates(item.getFrom(), item.getTo());
+				return new Booking(booking);
+			}
+		}
+		throw new ItemNotFoundException("Booking not found in repository!");
+	}
 
-    /**
-     * Removes an item from the repository
-     * by searching for an exact match.
-     * <p>
-     * Returns true if an exact match is and deleted,
-     * returns false if there's no match and the list is unchanged.
-     */
-    public boolean delete(Booking item) {
-        return repository.remove(item);
-    }
+	/**
+	 * Removes an item from the repository
+	 * by searching for an exact match.
+	 * <p>
+	 * Returns true if an exact match is and deleted,
+	 * returns false if there's no match and the list is unchanged.
+	 */
+	public boolean delete(Booking item) {
+		return repository.remove(item);
+	}
 
-    /**
-     * Removes the item in the repository
-     * matching the given id.
-     * <p>
-     * Returns true if there's a match and is deleted,
-     * returns false if there's no match and the list is unchanged.
-     */
-    public boolean deleteById(int id) {
-        Booking item = findById(id);
-        return delete(item);
-    }
+	/**
+	 * Removes the item in the repository
+	 * matching the given id.
+	 * <p>
+	 * Returns true if there's a match and is deleted,
+	 * returns false if there's no match and the list is unchanged.
+	 */
+	public boolean deleteById(int id) {
+		for (Booking booking : repository) {
+			if (booking.getGuestId() == id) {
+				return delete(booking);
+			}
+		}
+		return false;
+	}
 
-    /**
-     * Deletes all items in the repository
-     */
-    public void deleteAll() {
-        repository.clear();
-    }
+	/**
+	 * Deletes all items in the repository
+	 */
+	public void deleteAll() {
+		repository.clear();
+	}
 
-    /**
-     * Returns the number of items left in the repository
-     */
-    public int count() {
-        return repository.size();
-    }
+	/**
+	 * Returns the number of items left in the repository
+	 */
+	public int count() {
+		return repository.size();
+	}
+
+	private int idGenerator() {
+		return repository.get(count()).getBookingId() + 1;
+	}
 }
--- src/main/java/eu/deltasource/internship/hotel/repository/GuestRepository.java
+++ src/main/java/eu/deltasource/internship/hotel/repository/GuestRepository.java
@@ -13,121 +13,134 @@ import java.util.List;
  */
 public class GuestRepository {
 
-    private final List<Guest> repository;
+	private final List<Guest> repository;
 
-    /**
-     * Default constructor, which initializes the repository
-     * as an empty ArrayList.
-     */
-    public GuestRepository() {
-        repository = new ArrayList<>();
-    }
+	/**
+	 * Default constructor, which initializes the repository
+	 * as an empty ArrayList.
+	 */
+	public GuestRepository() {
+		repository = new ArrayList<>();
+	}
 
-    /**
-     * Returns an unmodifiable list of all items
-     * currently in the repository.
-     */
-    public List<Guest> findAll() {
-        return Collections.unmodifiableList(repository);
-    }
+	/**
+	 * Returns an unmodifiable list of all items
+	 * currently in the repository.
+	 */
+	public List<Guest> findAll() {
+		return Collections.unmodifiableList(repository);
+	}
 
-    /**
-     * Method, which checks the repository if
-     * there is an item available with the given id.
-     * <p>
-     * Check this always, before using operations with id's.
-     */
-    public boolean existsById(int id) {
-        for (Guest item : repository) {
-            if (item.getGuestId() == id)
-                return true;
-        }
-        return false;
-    }
+	/**
+	 * Method, which checks the repository if
+	 * there is an item available with the given id.
+	 * <p>
+	 * Check this always, before using operations with id's.
+	 */
+	public boolean existsById(int id) {
+		for (Guest item : repository) {
+			if (item.getGuestId() == id)
+				return true;
+		}
+		return false;
+	}
 
-    /**
-     * Returns a copy of the item from the repository
-     * with the given Id.
-     */
-    public Guest findById(int id) {
-        for (Guest item : repository) {
-            if (item.getGuestId() == id)
-                return new Guest(item);
-        }
-        throw new ItemNotFoundException("A Guest with id: " + id + " was not found!");
-    }
+	/**
+	 * Returns a copy of the item from the repository
+	 * with the given Id.
+	 */
+	public Guest findById(int id) {
+		for (Guest item : repository) {
+			if (item.getGuestId() == id)
+				return new Guest(item);
+		}
+		throw new ItemNotFoundException("A Guest with id: " + id + " was not found!");
+	}
 
-    /**
-     * Saves the item in the repository with a new id
-     * using the item count in the repository
-     */
-    public void save(Guest item) {
-        Guest newGuest = new Guest(count() + 1, item.getFirstName(), item.getLastName(), item.getGender());
-        repository.add(newGuest);
-    }
+	/**
+	 * Saves the item in the repository with a new id
+	 * using the item count in the repository
+	 */
+	public void save(Guest item) {
+		Guest newGuest = new Guest(idGenerator(), item.getFirstName(), item.getLastName(), item.getGender());
+		repository.add(newGuest);
+	}
 
-    /**
-     * Saves the list of items in the repository
-     */
-    public void saveAll(List<Guest> items) {
-        items.forEach(
-                this::save);
-    }
+	/**
+	 * Saves the list of items in the repository
+	 */
+	public void saveAll(List<Guest> items) {
+		items.forEach(
+			this::save);
+	}
 
-    /**
-     * Saves all given items in the repository
-     */
-    public void saveAll(Guest... items) {
-        saveAll(Arrays.asList(items));
-    }
+	/**
+	 * Saves all given items in the repository
+	 */
+	public void saveAll(Guest... items) {
+		saveAll(Arrays.asList(items));
+	}
 
-    /**
-     * Updates the names and gender of a given Guest
-     * All validations should be done in the service layer!!!
-     * <p>
-     * Returns a copy of the updated item
-     */
-    public Guest updateGuest(Guest item) {
-        Guest updatedGuest = findById(item.getGuestId());
-        updatedGuest.changeGender(item.getGender());
-        updatedGuest.changeFirstAndLastNames(item.getFirstName(), item.getLastName());
-        return new Guest(updatedGuest);
-    }
+	/**
+	 * Updates the names and gender of a given Guest
+	 * All validations should be done in the service layer!!!
+	 * <p>
+	 * Returns a copy of the updated item
+	 */
+	public Guest updateGuest(Guest item) {
+		for (Guest guest : repository) {
+			if (guest.getGuestId() == item.getGuestId()) {
+				guest.changeGender(item.getGender());
+				guest.changeFirstAndLastNames(item.getFirstName(), item.getLastName());
+				return new Guest(guest);
+			}
+		}
 
-    /**
-     * Removes an item from the repository
-     * by searching for an exact match.
-     * <p>
-     * Returns true if an exact match is and deleted,
-     * returns false if there's no match and the list is unchanged.
-     */
-    public boolean delete(Guest item) {
-        return repository.remove(item);
-    }
+		throw new ItemNotFoundException("Guest not found in repository!");
+	}
 
-    /**
-     * Removes the item in the repository
-     * matching the given id.
-     * <p>
-     * Returns true if there's a match and is deleted,
-     * returns false if there's no match and the list is unchanged.
-     */
-    public boolean deleteById(int id) {
-        Guest item = findById(id);
-        return delete(item);
-    }
+	/**
+	 * Removes an item from the repository
+	 * by searching for an exact match.
+	 * <p>
+	 * Returns true if an exact match is and deleted,
+	 * returns false if there's no match and the list is unchanged.
+	 */
+	public boolean delete(Guest item) {
+		return repository.remove(item);
+	}
 
-    /**
-     * Deletes all items in the repository
-     */
-    public void deleteAll() {
-        repository.clear();
-    }
+	/**
+	 * Removes the item in the repository
+	 * matching the given id.
+	 * <p>
+	 * Returns true if there's a match and is deleted,
+	 * returns false if there's no match and the list is unchanged.
+	 */
+	public boolean deleteById(int id) {
+		for (Guest guest : repository) {
+			if (guest.getGuestId() == id) {
+				return delete(guest);
+			}
+		}
+		return false;
+	}
 
-    /**
-     * Returns the number of items left in the repository
-     */
-    public int count() {
-        return repository.size();
-    }
+	/**
+	 * Deletes all items in the repository
+	 */
+	public void deleteAll() {
+		repository.clear();
+	}
+
+	/**
+	 * Returns the number of items left in the repository
+	 */
+	public int count() {
+		return repository.size();
+	}
+
+	private int idGenerator() {
+		return repository.get(count()).getGuestId() + 1;
+	}
 }
