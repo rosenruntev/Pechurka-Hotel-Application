7eae67ba12ae6bf0707116ecbe1079399a4d3b09
Rosen Runtev <runtev.rosen@gmail.com>
2019-07-30 09:02:20 +0300

    Added createBooking method test cases in BookingServiceTest

--- src/test/java/eu/deltasource/internship/hotel/service/BookingServiceTest.java
+++ src/test/java/eu/deltasource/internship/hotel/service/BookingServiceTest.java
@@ -1,5 +1,13 @@
 package eu.deltasource.internship.hotel.service;
 
+import eu.deltasource.internship.hotel.domain.Booking;
+import eu.deltasource.internship.hotel.domain.Gender;
+import eu.deltasource.internship.hotel.domain.Guest;
+import eu.deltasource.internship.hotel.domain.Room;
+import eu.deltasource.internship.hotel.domain.commodity.AbstractCommodity;
+import eu.deltasource.internship.hotel.domain.commodity.Bed;
+import eu.deltasource.internship.hotel.domain.commodity.BedType;
+import eu.deltasource.internship.hotel.exception.ItemNotFoundException;
 import eu.deltasource.internship.hotel.repository.BookingRepository;
 import eu.deltasource.internship.hotel.repository.GuestRepository;
 import eu.deltasource.internship.hotel.repository.RoomRepository;
@@ -7,7 +15,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BookingServiceTest {
@@ -24,16 +35,15 @@ public class BookingServiceTest {
 	@BeforeEach
 	public void setUp() {
 		roomRepository = new RoomRepository();
-		roomService = new RoomService(roomRepository);
-
 		guestRepository = new GuestRepository();
-		guestService = new GuestService(guestRepository);
-
 		bookingRepository = new BookingRepository();
+
+		roomService = new RoomService(roomRepository);
+		guestService = new GuestService(guestRepository);
 		bookingService = new BookingService(bookingRepository, roomService, guestService);
 
-		bookingFromDate = LocalDate.of(2019, 7, 20);
-		bookingToDate = LocalDate.of(2019, 7, 25);
+		bookingFromDate = LocalDate.now();
+		bookingToDate = bookingFromDate.plusDays(5);
 	}
 
 	@Test
@@ -42,5 +52,120 @@ public class BookingServiceTest {
 			bookingService.createBooking(-1, 1, 1, 1,
 				bookingFromDate, bookingToDate);
 		});
+	}
+
+	@Test
+	public void createBookingShouldThrowExceptionIfBookingIdAlreadyExists() {
+		Booking booking = new Booking(1, 1, 1, 1,
+			bookingFromDate, bookingToDate);
+		bookingRepository.save(booking);
+
+		assertThrows(IllegalArgumentException.class, () -> {
+			bookingService.createBooking(1, 1, 1, 1,
+				bookingFromDate, bookingToDate);
+		});
+	}
+
+	@Test
+	public void createBookingShouldThrowExceptionIfGuestDoesNotExist() {
+		assertThrows(ItemNotFoundException.class, () -> {
+			bookingService.createBooking(1, 1, 1, 1,
+				bookingFromDate, bookingToDate);
+		});
+	}
+
+	@Test
+	public void createBookingShouldThrowExceptionIfRoomDoesNotExist() {
+		Guest guest = new Guest(1, "first name", "last name", Gender.MALE);
+		guestRepository.save(guest);
+
+		assertThrows(ItemNotFoundException.class, () -> {
+			bookingService.createBooking(1, 1, 1, 1,
+				bookingFromDate, bookingToDate);
+		});
+	}
+
+	@Test
+	public void createBookingShouldThrowExceptionIfRoomHasDifferentCapacity() {
+		Guest guest = new Guest(1, "first name", "last name", Gender.MALE);
+		guestRepository.save(guest);
+
+		Set<AbstractCommodity> commodities = new HashSet();
+		commodities.add(new Bed(BedType.DOUBLE));
+		Room room = new Room(1, commodities);
+		roomService.saveRoom(room);
+
+		assertThrows(IllegalArgumentException.class, () -> {
+			bookingService.createBooking(1, 1, 1, 1,
+				bookingFromDate, bookingToDate);
+		});
+	}
+
+	@Test
+	public void createBookingShouldThrowExceptionIfDatesAreEqual() {
+		Guest guest = new Guest(1, "first name", "last name", Gender.MALE);
+		guestRepository.save(guest);
+
+		Set<AbstractCommodity> commodities = new HashSet();
+		commodities.add(new Bed(BedType.DOUBLE));
+		Room room = new Room(1, commodities);
+		roomService.saveRoom(room);
+
+		assertThrows(IllegalArgumentException.class, () -> {
+			bookingService.createBooking(1, 1, 1, 2,
+				LocalDate.now(), LocalDate.now());
+		});
+	}
+
+	@Test
+	public void createBookingShouldThrowExceptionIfFromDateIsAfterToDate() {
+		Guest guest = new Guest(1, "first name", "last name", Gender.MALE);
+		guestRepository.save(guest);
+
+		Set<AbstractCommodity> commodities = new HashSet();
+		commodities.add(new Bed(BedType.DOUBLE));
+		Room room = new Room(1, commodities);
+		roomService.saveRoom(room);
+
+		assertThrows(IllegalArgumentException.class, () -> {
+			bookingService.createBooking(1, 1, 1, 2,
+				LocalDate.now().plusDays(1), LocalDate.now());
+		});
+	}
+
+	@Test
+	public void createBookingShouldThrowExceptionIfRoomIsBookedForThatPeriod() {
+		Guest guest = new Guest(1, "first name", "last name", Gender.MALE);
+		guestRepository.save(guest);
+
+		Set<AbstractCommodity> commodities = new HashSet();
+		commodities.add(new Bed(BedType.DOUBLE));
+		Room room = new Room(1, commodities);
+		roomService.saveRoom(room);
+
+		Booking booking = new Booking(1, 1, 1, 1,
+			bookingFromDate, bookingToDate);
+		bookingRepository.save(booking);
+
+		assertThrows(IllegalArgumentException.class, () -> {
+			bookingService.createBooking(2, 1, 1, 2,
+				bookingFromDate, bookingToDate);
+		});
+	}
+
+	@Test
+	public void createBookingShouldCreateNewBookingInBookingRepository() {
+		Guest guest = new Guest(1, "first name", "last name", Gender.MALE);
+		guestRepository.save(guest);
+
+		Set<AbstractCommodity> commodities = new HashSet();
+		commodities.add(new Bed(BedType.DOUBLE));
+		Room room = new Room(1, commodities);
+		roomService.saveRoom(room);
+
+		bookingService.createBooking(1, 1, 1, 2,
+			bookingFromDate, bookingToDate);
+
+		assertEquals(true, bookingRepository.existsById(1));
 	}
 }
