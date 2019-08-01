705eb4bba98f2fb9d76aa008006cf932ca6d7b84
Rosen Runtev <runtev.rosen@gmail.com>
2019-08-01 11:51:59 +0300

    Added javadoc to existsById in GuestService

--- src/main/java/eu/deltasource/internship/hotel/service/GuestService.java
+++ src/main/java/eu/deltasource/internship/hotel/service/GuestService.java
@@ -29,6 +29,11 @@ public class GuestService {
 		return guestRepository.findById(id);
 	}
 
+	/**
+	 * Returns if the guest with particular id exists
+	 * @param id the id of the guest
+	 * @return true if the guest exists otherwise false
+	 */
 	public boolean existsById(int id) {
 		return guestRepository.existsById(id);
 	}
