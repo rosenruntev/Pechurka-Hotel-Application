792a5cb1b7b565b0d39fe1b21096cd515826a658
Rosen Runtev <runtev.rosen@gmail.com>
2019-08-01 14:09:42 +0300

    Added BookingController Post

--- src/main/java/eu/deltasource/internship/hotel/controller/BookingController.java
+++ src/main/java/eu/deltasource/internship/hotel/controller/BookingController.java
@@ -1,14 +1,27 @@
 package eu.deltasource.internship.hotel.controller;
 
 import eu.deltasource.internship.hotel.domain.Booking;
+import eu.deltasource.internship.hotel.domain.Gender;
+import eu.deltasource.internship.hotel.domain.Guest;
+import eu.deltasource.internship.hotel.domain.Room;
+import eu.deltasource.internship.hotel.domain.commodity.AbstractCommodity;
+import eu.deltasource.internship.hotel.domain.commodity.Bed;
+import eu.deltasource.internship.hotel.domain.commodity.BedType;
 import eu.deltasource.internship.hotel.repository.*;
 import eu.deltasource.internship.hotel.service.*;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 public class BookingController {
@@ -29,6 +42,19 @@ public class BookingController {
 		guestService = new GuestService(guestRepository);
 		roomService = new RoomService(roomRepository);
 		bookingService = new BookingService(bookingRepository, roomService, guestService);
+
+		Set<AbstractCommodity> commodities = new HashSet<>();
+		commodities.add(new Bed(BedType.SINGLE));
+		roomRepository.save(new Room(1, commodities));
+		guestRepository.save(new Guest(1, "f", "l", Gender.MALE));
+		bookingRepository.save(new Booking(1, 1, 1,
+			1, LocalDate.now(), LocalDate.now().plusMonths(1)));
+	}
+
+	@RequestMapping(value = "/booking", method = POST)
+	public void createBooking(@RequestBody Booking booking) {
+		bookingService.createBooking(booking.getBookingId(), booking.getGuestId(), booking.getRoomId(),
+			booking.getNumberOfPeople(), booking.getFrom(), booking.getTo());
 	}
 
 	@RequestMapping(value = "/booking", method = GET)
