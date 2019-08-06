b93f570f15f63bdd0242e668dcb1bdc324495b28
Rosen Runtev <runtev.rosen@gmail.com>
2019-08-06 16:29:27 +0300

    Added Booking transfer object

---
+++ src/main/java/eu/deltasource/internship/hotel/transferobject/BookingTO.java
@@ -0,0 +1,49 @@
+package eu.deltasource.internship.hotel.transferobject;
+
+import java.time.LocalDate;
+
+public class BookingTO {
+	private int bookingId;
+	private int guestId;
+	private LocalDate fromDate;
+	private LocalDate toDate;
+
+	public BookingTO(int bookingId, int guestId, LocalDate fromDate, LocalDate toDate) {
+		this.bookingId = bookingId;
+		this.guestId = guestId;
+		this.fromDate = fromDate;
+		this.toDate = toDate;
+	}
+
+	public int getBookingId() {
+		return bookingId;
+	}
+
+	public void setBookingId(int bookingId) {
+		this.bookingId = bookingId;
+	}
+
+	public int getGuestId() {
+		return guestId;
+	}
+
+	public void setGuestId(int guestId) {
+		this.guestId = guestId;
+	}
+
+	public LocalDate getFromDate() {
+		return fromDate;
+	}
+
+	public void setFromDate(LocalDate fromDate) {
+		this.fromDate = fromDate;
+	}
+
+	public LocalDate getToDate() {
+		return toDate;
+	}
+
+	public void setToDate(LocalDate toDate) {
+		this.toDate = toDate;
+	}
+}
