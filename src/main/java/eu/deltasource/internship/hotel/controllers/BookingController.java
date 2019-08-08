5a0e214724a8e48714dc1bb42f288940527b6937
Rosen Runtev <runtev.rosen@gmail.com>
2019-08-08 10:55:39 +0300

    Reformated files

--- src/main/java/eu/deltasource/internship/hotel/controllers/BookingController.java
+++ src/main/java/eu/deltasource/internship/hotel/controllers/BookingController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
 import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.*;
--- src/test/java/eu/deltasource/internship/hotel/controllers/GuestControllerTest.java
+++ src/test/java/eu/deltasource/internship/hotel/controllers/GuestControllerTest.java
@@ -3,8 +3,6 @@ package eu.deltasource.internship.hotel.controllers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class GuestControllerTest {
 
 	@AfterEach
